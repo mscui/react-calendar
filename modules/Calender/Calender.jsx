c388bba52d59225eac64f070bbceacd2095cce1b
Candybunny <cuixuefei31@gmail.com>
2016-11-17 15:11:02 +0800

    complete canlendar

--- modules/Calender/Calender.jsx
+++ modules/Calender/Calender.jsx
@@ -19,23 +19,29 @@ let util = {
 			default:
 				return o.year + '-' + o.month + '-' + o.day;
 		}
-	},
-	bodyDomEvent() {
-		$(document).bind('click', function(e) {
-			$('.header-year-list').hide();
-			$('.header-month-list').hide();
-		});
 	}
 };
 class CalShow extends React.Component {
 	constructor(props) {
 	    super(props);
 	}
+	handleClick(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.nativeEvent.stopImmediatePropagation();
+		$('.cal-panel').toggle();
+	}
+	componentDidMount() {
+		$(document).on('click', function(e){
+            e.preventDefault();
+            $('.cal-panel').hide();
+        });
+	}
 	render() {
 		return (
 			<div className="cal-show">
 				<span>{util.formatDate(this.props.fromdate)}-{util.formatDate(this.props.enddate)}</span>
-				<a href="" className="cal-btn"></a>
+				<a href="" className="cal-btn" onClick={this.handleClick}></a>
 			</div>
 		);
 	};
@@ -46,15 +52,19 @@ class CalHeader extends React.Component {
 
 		this.state = {
 			month: this.props.month,
-			year: this.props.year
+			year: this.props.year,
+			fromdate: this.props.fromdate,
+			enddate: this.props.enddate
 		}
 	    this.handleLeftClick = this.handleLeftClick.bind(this);
 	    this.handleRightClick = this.handleRightClick.bind(this);
 	}
 	handleLeftClick(e) {
 		e.preventDefault();
-		var newMonth = this.state.month - 1;
-		var year = this.state.year;
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+		var newMonth = this.props.month - 1;
+		var year = this.props.year;
 		var day = this.props.day;
 
 		if(newMonth < 1){
@@ -63,23 +73,26 @@ class CalHeader extends React.Component {
 		}
 		this.state.month = newMonth;
 		this.state.year = year;
-		this.setState(this.state);
 		if (this.props.name === 'from') {
-			var filterfrom = new Date(year, newMonth - 1, day);
-			var filterend = this.props.enddate;
+			this.state.fromdate = new Date(year, newMonth - 1, day);
+			this.state.enddate = this.props.enddate;
 		} else {
-			var filterfrom = this.props.fromdate;
-			var filterend = new Date(year, newMonth - 1, day);
+			this.state.fromdate = this.props.fromdate;
+			this.state.enddate = new Date(year, newMonth - 1, day);
+			if(this.state.fromdate.getTime() > this.state.enddate.getTime()) {
+				this.state.fromdate = this.state.enddate;
+			}
 		}
+		this.setState(this.state);
 		this.props.updateFilter(year, newMonth);
-		console.log(filterfrom);
-		this.props.updateCtrlFilter(filterfrom, filterend);
+		this.props.updateCtrlFilter(this.state.fromdate, this.state.enddate);
 	}
 	handleRightClick(e) {
 		e.preventDefault();
-
-		var newMonth = this.state.month + 1;
-		var year = this.state.year;
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+		var newMonth = this.props.month + 1;
+		var year = this.props.year;
 		var day = this.props.day;
 
 		if( newMonth > 12 ){
@@ -89,27 +102,32 @@ class CalHeader extends React.Component {
 
 		this.state.month = newMonth;
 		this.state.year = year;
-		this.setState(this.state);
 		if (this.props.name === 'from') {
-			var filterfrom = new Date(year, newMonth - 1, day);
-			var filterend = this.props.enddate;
+			this.state.fromdate = new Date(year, newMonth - 1, day);
+			this.state.enddate = this.props.enddate;
+			if(this.state.fromdate.getTime() > this.state.enddate.getTime()) {
+				this.state.enddate = this.state.fromdate;
+			}
 		} else {
-			var filterfrom = this.props.fromdate;
-			var filterend = new Date(year, newMonth - 1, day);
+			this.state.fromdate = this.props.fromdate;
+			this.state.enddate = new Date(year, newMonth - 1, day);
 		}
+		this.setState(this.state);
 		// 子组件变化渲染到父组件
 		this.props.updateFilter(year, newMonth);
-		this.props.updateCtrlFilter(filterfrom, filterend);
+		this.props.updateCtrlFilter(this.state.fromdate, this.state.enddate);
 	}
 	showYearList(e) {
 		e.preventDefault();
 		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
 		var $yearList = $(e.target).parent().find('.header-year-list');
 		$yearList.toggle();
 	}
 	showMonthList(e) {
 		e.preventDefault();
 		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
 		var $monthList = $(e.target).parent().find('.header-month-list');
 		$monthList.toggle();
 	}
@@ -122,9 +140,7 @@ class CalHeader extends React.Component {
 		for (let y = 1; y < 13; y++) {
 			monthArray.push(<li key={y}>{y}</li>);
 		}
-		// className看成变量，用end和from日期来判断按钮是否可点击
-		// className = {beforebtn}
-		// var beforebtn = 'header-before'; if (before > end) beforebtn += ' unable' ? '';
+
 		return (
 			<div className="header">
 				<a href="" className="header-before" onClick={this.handleLeftClick}>before</a>
@@ -145,6 +161,7 @@ class CalHeader extends React.Component {
 class CalBody extends React.Component {
 	constructor(props) {
 	    super(props);
+	    this.handleDayClick = this.handleDayClick.bind(this);
 	}
 	getMonthDays() {
 		var year = this.props.year;
@@ -159,9 +176,25 @@ class CalBody extends React.Component {
 		var weekdays = dt.getDay();
 		return weekdays;
 	}
-	handleDayClick() {
+	handleDayClick(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+		var filterfrom = this.props.fromdate;
+		var filterend = this.props.enddate;
 		// 点击日期，变更dur的时间
-		// this.props.updateCtrlFilter(filterfrom, filterend);
+		if (this.props.name === 'from') {
+			filterfrom = new Date(this.props.year + ',' + this.props.month + ',' + $(e.target).attr('data-key'));
+			if(filterfrom.getTime() > filterend.getTime()) {
+				filterend = filterfrom;
+			}
+		} else {
+			filterend = new Date(this.props.year + ',' + this.props.month + ',' + $(e.target).attr('data-key'));
+			if(filterend.getTime() < filterfrom.getTime()) {
+				filterfrom = filterend;
+			}
+		}
+		this.props.updateCtrlFilter(filterfrom, filterend);
 	}
 	render() {
 		var arry1 =[];
@@ -169,14 +202,38 @@ class CalBody extends React.Component {
 		var getDays = this.getMonthDays();
 		var	FirstDayWeek = this.getFirstDayWeek();
 		var	day = this.props.day;
+		var fromdate = util.formatDate(this.props.fromdate);
+		var enddate = util.formatDate(this.props.enddate);
+		var fromyear = util.formatDate(this.props.fromdate, 'yyyy');
+		var endyear = util.formatDate(this.props.enddate, 'yyyy');
+		var frommonth = util.formatDate(this.props.fromdate, 'mm');
+		var endmonth = util.formatDate(this.props.enddate, 'mm');
+		var fromday = util.formatDate(this.props.fromdate, 'dd');
+		var endday = util.formatDate(this.props.enddate, 'dd');
 		for(var i = 0; i < FirstDayWeek; i++ ){
 			arry1[i] = i;
 		}
 		for(var i = 0; i < getDays; i++ ){
 			arry2[i] = (i + 1);
 		}
-		var node1 = arry1.map(function(item){return <li key={item}></li>});
-		var node2 = arry2.map(function(item){return (day === item) ? <li className="on" key={item}>{item}</li> : <li key={item}>{item}</li>});
+		var node1 = arry1.map(function(item){
+			return <li key={item} onClick={this.handleDayClick}></li>
+		}.bind(this));
+		var node2 = arry2.map(function(item){
+			if (day === item) {
+				return <li className="on" key={item} data-key={item}>{item}</li>
+			} else if (this.props.name === 'from' && fromdate !== enddate && frommonth !== endmonth && item > fromday) {
+				return <li key={item} className="dur" data-key={item} onClick={this.handleDayClick}>{item}</li>
+			} else if (this.props.name === 'end' && fromdate !== enddate && frommonth !== endmonth && item < endday) {
+				return <li key={item} className="dur" data-key={item} onClick={this.handleDayClick}>{item}</li>
+			} else if (this.props.name === 'from' && fromdate !== enddate && frommonth == endmonth && item <= endday && item > fromday) {
+				return <li key={item} className="dur" data-key={item} onClick={this.handleDayClick}>{item}</li>
+			} else if (this.props.name === 'end' && fromdate !== enddate && frommonth == endmonth && item >= fromday && item < endday) {
+				return <li key={item} className="dur" data-key={item} onClick={this.handleDayClick}>{item}</li>
+			} else {
+				return <li key={item} data-key={item} onClick={this.handleDayClick}>{item}</li>
+			}
+		}.bind(this));
 
 		return (
 			<div className="cal-body">
@@ -197,11 +254,31 @@ class CalBody extends React.Component {
 	};
 }
 class CalFooter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleComfirm = this.handleComfirm.bind(this);
+		this.handleCancle = this.handleCancle.bind(this);
+	}
+	handleComfirm(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+		var fromdate = Date.parse(this.props.fromdate) / 1000;
+		var enddate = Date.parse(this.props.enddate) / 1000;
+		console.log(fromdate + '-' + enddate);
+		$('.cal-panel').hide();
+	}
+	handleCancle(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+		$('.cal-panel').hide();
+	}
 	render() {
 		return (
 			<div className="cal-footer">
-				<a href="">确定</a>
-				<a href="">取消</a>
+				<a href="" onClick={this.handleComfirm}>确定</a>
+				<a href="" onClick={this.handleCancle}>取消</a>
 			</div>
 		);
 	}
@@ -268,18 +345,13 @@ class CalControl extends React.Component {
 			fromdate: fromdate,
 			enddate: enddate
 		};
-		util.bodyDomEvent();
 		this.updateCtrlFilter = this.updateCtrlFilter.bind(this);
 	}
 	updateCtrlFilter(filterfrom, filterend) {
-		if(filterfrom.getTime() > filterend.getTime()) {
-			filterend = filterfrom;
-		}
 		this.setState({
 	    	enddate: filterend,
 	    	fromdate: filterfrom
 	    });
-
 	}
 	render() {
 
@@ -303,7 +375,10 @@ class CalControl extends React.Component {
 						enddate={this.state.enddate}
 						updateCtrlFilter={this.updateCtrlFilter}
 					/>
-					<CalFooter />
+					<CalFooter
+						fromdate={this.state.fromdate}
+						enddate={this.state.enddate}
+					/>
 				</div>
 			</div>
 		);
